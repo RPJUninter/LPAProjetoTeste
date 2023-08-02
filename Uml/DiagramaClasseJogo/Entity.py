6f61f000fafbf0574cf08ccf4e5e411e9958f2a0
RenanJorge <renan.j@uninter.com>
2023-08-02 10:16:40 -0300

    Somente Diagrama de Classes
    Signed-off-by: RenanJorge <renan.j@uninter.com>

---
+++ Uml/DiagramaClasseJogo/Entity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+        self.mov_delay = None
+
+    def move(self, ):
+        pass
+

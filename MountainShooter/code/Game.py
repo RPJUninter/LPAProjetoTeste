227752aace62eaeb4556b0820e88c6d929555d4a
RenanJorge <renan.j@uninter.com>
2023-08-02 10:29:02 -0300

    Classes geradas a partir do UML

---
+++ MountainShooter/code/Game.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Game:
+    def __init__(self):
+        self.window = None
+        self.level_list = None
+        self.menu = None
+
+    def loop(self, ):
+        pass
+
